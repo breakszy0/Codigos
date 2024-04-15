2ae09a0a05a50427b4a93899ddb8e79ce4ec9f9c
breakszy0 <166059785+breakszy0@users.noreply.github.com>
2024-04-15 18:07:59 -0400

    Add files via upload

---
+++ introduccion-a-python.py
@@ -0,0 +1,52 @@
+#Comentario de una linea
+
+""" 
+    
+    Comentarios multilinea
+    
+    Que es una variable: Es un espacio de memoria reservado. que puede
+    cambiar en el transcurso del programa.
+    
+    Es un lenguaje de tipo dinamico: que no es necesario declarar
+    explicitamente el tipo de variable antes de usarla.
+    El tipo de dato se infiere segun el valor
+    
+"""
+    
+entero = 5
+#Para imprimir dato de python se utiliza la funcion print
+print(entero)
+#Para saber el tipo de dato de una variable type()
+print(type[entero])
+    
+#Para declarar variable de python: se utilizan letras, numeros, guionbajo
+#Debe comenzar por una letra
+#Case sensitive
+variableUno = 1
+variableuno = 2
+
+"""
+Palabras reservadas en Python:
+False, None, True, and, as, assert, async, await, break, class, continue, def, del, elif,
+else, except, finally, for, global, if, inport, in, is, lambda, nonLocal, not, or, pass,
+raise, return, try, while, whith y yield.   
+"""
+
+#Tipos de datos de python
+
+verEntero = 2
+flotante = 1,75
+booleanoTrue = True
+booleanoFalse = False
+cadenaTexto = "Soy una cadena de texto"
+
+suma = 4+10
+resta = 10-4
+divicion = 10/2
+multiplicacion = 6*6 
+
+
+print(suma)
+print(resta)
+print(divicion)
+print(multiplicacion)
