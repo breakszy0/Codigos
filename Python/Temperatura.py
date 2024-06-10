edb6d2597232a8b1578b4daca72d2c7ac630cb8d
breakszy0 <166059785+breakszy0@users.noreply.github.com>
2024-06-10 15:46:22 -0400

    Delete Python/Temperatura.py

--- Python/Temperatura.py
+++
@@ -1,7 +0,0 @@
-celsius_str = input("Ingresa la temperatura en grados Celsius: ")
-
-celsius = float(celsius_str)
-
-fahrenheit = (celsius * 9/5) + 32
-
-print("La temperatura en grados Fahrenheit es:", fahrenheit)
